253fa8654313b4a0bfc83541636dde1fd0526aee
Yossueth <patricksarguedas2806@gmail.com>
2024-06-03 15:50:32 -0600

    configuracion de botones y el input para poder agregar tareas

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
-// Inserte el código aquí
+let btn = document.querySelector(".btn");
+let input = document.querySelector("input");
+let ul = document.querySelector("ul");
+let textoTareas = document.querySelector(".textoTareas");
 
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let text = input.value;
+  let li = document.querySelector("li");
+  let texto1 = document.querySelector(".texto1");
+  texto1.textContent = text;
+
+  li.appendChild(p);
+  ul.appendChild(li);
+});
